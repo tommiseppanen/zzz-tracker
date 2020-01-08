aa91d3c8d339154d93e6e82c490ad9350d9837a6
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2020-01-08 13:08:50 +0200

    Add log component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ActionBar from './components/ActionBar';
+import Log from './components/Log';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       test
+      <Log/>
       <ActionBar/>
     </div>
   );
---
+++ src/components/Log.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+const Log: React.FC = () => {
+  const [content, setContent] = React.useState("");
+  return (
+    <div>
+      😴
+      👀😴
+      👀😴
+      👀😴
+      👀
+    </div>
+  );
+};
+
+export default Log;
