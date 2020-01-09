84feef0699979442d33758c4ab57318688fa7599
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2020-01-09 12:56:55 +0200

    Basic styles for ActionBar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
+import './ActionBar.css';
 
 const ActionBar: React.FC = () => {
   const [count, setCount] = React.useState(0);
   return (
     <div className="action-bar">
-      <div><span>button 1</span></div>
-      <div><span>button 2</span></div>
-      <div><span>button 3</span></div>
+      <div className="action-bar__button"><span>button 1</span></div>
+      <div className="action-bar__button"><span>button 2</span></div>
+      <div className="action-bar__button"><span>button 3</span></div>
     </div>
   );
 };
