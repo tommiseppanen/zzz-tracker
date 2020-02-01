3391bc016756019fb61c778cb730ae3ce93e20c4
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2020-02-01 04:11:49 +0200

    Move init to same place with other actions

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,12 @@ import ActionBar from './components/ActionBar';
 import Log from './components/Log';
 import sleepEventReducer from './logic/sleepEventReducer';
 import  * as actions from './logic/actions';
-import SleepEventDatabase from './models/SleepEventDatabase';
 
 const App: React.FC = () => {
   const [sleepEvents, dispatchSleepEvents] = React.useReducer(sleepEventReducer, { sleepEvents: [] });
 
   React.useEffect(() => {
-    const db = new SleepEventDatabase();
-    db.sleepEvents.toArray().then(events => {
-      console.log(events);
-      dispatchSleepEvents({ type: 'init', payload: events });
-    });
+    actions.initSleepEvents(dispatchSleepEvents);
   }, []);
 
   return (
--- src/logic/actions.ts
+++ src/logic/actions.ts
@@ -2,45 +2,51 @@ import {db} from './../models/SleepEventDatabase';
 import SleepEvent, { SleepState } from './../models/SleepEvent';
 import * as sleepEventReducer from '../logic/sleepEventReducer';
 
+export function initSleepEvents(dispatch: React.Dispatch<sleepEventReducer.SleepActionType>) {
+  db.sleepEvents.toArray().then(events => {
+    dispatch({ type: 'init', payload: events });
+  });
+}
+
 export function addAwake(dispatch: React.Dispatch<sleepEventReducer.SleepActionType>) {
-    return () => {
-        const eventToAdd = new SleepEvent(new Date(), SleepState.Awake);
-        db.sleepEvents
-          .add(eventToAdd)
-          .then((id) => {
-             dispatch({
-               type: 'add',
-               payload: [eventToAdd],
-             });
-          });
-    }
+  return () => {
+    const eventToAdd = new SleepEvent(new Date(), SleepState.Awake);
+    db.sleepEvents
+      .add(eventToAdd)
+      .then((id) => {
+        dispatch({
+          type: 'add',
+          payload: [eventToAdd],
+        });
+      });
+  }
 }
 
 export function addAsleep(dispatch: React.Dispatch<sleepEventReducer.SleepActionType>) {
-    return () => {
-        const eventToAdd = new SleepEvent(new Date(), SleepState.Asleep);
-        db.sleepEvents
-          .add(eventToAdd)
-          .then((id) => {
-             dispatch({
-               type: 'add',
-               payload: [eventToAdd],
-             });
-          });
-    }
+  return () => {
+    const eventToAdd = new SleepEvent(new Date(), SleepState.Asleep);
+    db.sleepEvents
+      .add(eventToAdd)
+      .then((id) => {
+        dispatch({
+          type: 'add',
+          payload: [eventToAdd],
+        });
+      });
+  }
 }
 
 export function addAwakeAndAsleep(dispatch: React.Dispatch<sleepEventReducer.SleepActionType>) {
-    return () => {
-        const awakeEvent = new SleepEvent(new Date(), SleepState.Awake);
-        const asleepEvent = new SleepEvent(new Date(), SleepState.Asleep);
-        db.sleepEvents
-          .bulkAdd([awakeEvent,asleepEvent])
-          .then((id) => {
-             dispatch({
-               type: 'add',
-               payload: [awakeEvent,asleepEvent],
-             });
-          });
-    }
+  return () => {
+    const awakeEvent = new SleepEvent(new Date(), SleepState.Awake);
+    const asleepEvent = new SleepEvent(new Date(), SleepState.Asleep);
+    db.sleepEvents
+      .bulkAdd([awakeEvent,asleepEvent])
+      .then((id) => {
+        dispatch({
+          type: 'add',
+          payload: [awakeEvent,asleepEvent],
+        });
+      });
+  }
 }
